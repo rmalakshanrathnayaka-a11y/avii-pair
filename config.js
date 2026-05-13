30c8d62b0214aeb6eaa3b4b7edc90869bcd7ae64
rmalakshanrathnayaka-a11y <r.m.a.lakshanrathnayaka@gmail.com>
2026-05-13 10:58:18 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+global.prefix = [".", "!", ".", ",", "🐤", "🗿"]; 
+global.publik = true
+global.owner = ["6283115153427"] 
+global.owner2 = ["6383830206761"] 
+global.owner3 = [""] 
+global.owner4 = [""] 
+global.namabot = 'Aethernox Infinity'
+//======================
+global.mess = { 
+owner: 'Minta akses ke YanzzOfficial ',
+premium: 'Minta Akses Ke YanzzOfficial',
+succes: 'Success by YanzzOfficial'
+}
+//======================
